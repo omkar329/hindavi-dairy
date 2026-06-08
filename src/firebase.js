eabee0f9adc45bfa15a2b918d4388cc7194a97a8
unknown <okaunsalye@gmail.com>
2026-06-08 20:02:34 +0530

    Remove BOM from firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-﻿import { initializeApp } from "firebase/app";
+/* eslint-disable unicode-bom */
+
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
